c3cb3f8cc23fd462c0cfc53d77e8fa3d3c9fea61
AucerolaNocry <setterurlk@gmail.com>
2025-05-20 20:24:27 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -6,29 +6,46 @@ $SRC = "/storage/emulated/0/Pictures/TESTE/PINS/PINSSALVOS/com.dts.freefireth";
 $DEST = "/storage/emulated/0/Android/data/com.dts.freefireth";
 $DATA = "20240501";
 
-// Etapas
-shell_exec("adb shell cp -rf '$SRC' '/storage/emulated/0/Android/data/'");
+// 1. Copiar pasta
+echo "[*] Copiando pasta para Android/data...\n";
+shell_exec("adb shell cp -rf '$SRC' '$DEST'");
 
-// Arquivos a tratar
+// 2. Arquivos a camuflar
 $ARQUIVOS = [
-    "$DEST/files/contentcache/optional/android/gameassetbundles/shaders.fake" => "1055.00"
+    "$DEST/files/ShaderStripSettings" => "0930.00",
+    "$DEST/files" => "0945.00",
+    "$DEST/files/contentcache" => "1005.00",
+    "$DEST/files/contentcache/optional" => "1015.00",
+    "$DEST/files/contentcache/optional/android" => "1025.00",
+    "$DEST/files/contentcache/optional/android/gameassetbundles" => "1035.00",
+    "$DEST" => "1045.00",
+    "$DEST/files/contentcache/optional/android/gameassetbundles/shaders.fake" => "1055.00",
+    "$DEST/files/ffrtc_log.txt" => "2300.00"
 ];
 
-// 1. Criar e injetar conteúdo UnityFS + padronizar data modificação/criação
+// 3. Criar arquivos e aplicar datas
 foreach ($ARQUIVOS as $arquivo => $hora) {
-    shell_exec("adb shell "echo 'UnityFS' > $arquivo"");
-    shell_exec("adb shell touch -t {$DATA}{$hora} $arquivo");
-    echo "[✓] Arquivo UnityFS criado e camuflado: $arquivo\n";
+    if (strpos($arquivo, 'shaders.fake') !== false) {
+        shell_exec("adb shell "echo 'UnityFS' > $arquivo"");
+    } else {
+        shell_exec("adb shell "touch $arquivo"");
+    }
 
-    // Forçar 'stat -c %z' igual a %y simulando recriação imediata
-    $cmdResetInode = "adb shell "mv $arquivo $arquivo.tmp && mv $arquivo.tmp $arquivo"";
-    shell_exec($cmdResetInode);
+    shell_exec("adb shell touch -t {$DATA}{$hora} $arquivo");
+
+    // Forçar regravação para igualar %y e %z
+    shell_exec("adb shell "mv $arquivo $arquivo.tmp && mv $arquivo.tmp $arquivo"");
+    echo "[✓] Arquivo camuflado: $arquivo\n";
 }
 
-// 2. Abrir Free Fire e Discord
+// 4. Abrir Free Fire
+echo "[*] Abrindo Free Fire...\n";
 shell_exec("adb shell monkey -p com.dts.freefireth -c android.intent.category.LAUNCHER 1");
 sleep(5);
+
+// 5. Abrir Discord
+echo "[*] Abrindo Discord...\n";
 shell_exec("adb shell monkey -p com.discord -c android.intent.category.LAUNCHER 1");
 
-echo "====== CAMUFLAGEM FINALIZADA ======\n";
+echo "====== CAMUFLAGEM COMPLETA ======\n";
 ?>
